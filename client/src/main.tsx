536d11a443b4068fed8770c88cef990053199f1c
paul-nguyen96 <paul121596@icloud.com>
2025-04-05 16:12:56 -0500

    mantine working

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
+import { Chip, createTheme, FileInput, MantineProvider } from "@mantine/core";
+import "./styles/index.css";
+import "@mantine/core/styles.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+const rootElement = document.getElementById("root")!;
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <MantineProvider theme={theme}>
+        <FileInput
+          label="Input label"
+          description="Input description"
+          placeholder="Input placeholder"
+        />
+        <Chip defaultChecked>Awesome chip</Chip>
+      </MantineProvider>
+    </StrictMode>
+  );
+}
