199f5b82ef05dc7c56ddf3be460fef409abd38d5
paul-nguyen96 <paul121596@icloud.com>
2025-04-05 22:30:09 -0500

    navbar remove boiler plate

--- client/src/components/mantine/navbar.tsx
+++ client/src/components/mantine/navbar.tsx
@@ -1,15 +1,5 @@
 import { useEffect, useState } from "react";
-import {
-  IconChevronDown,
-  IconHeart,
-  IconLogout,
-  IconMessage,
-  IconPlayerPause,
-  IconSettings,
-  IconStar,
-  IconSwitchHorizontal,
-  IconTrash,
-} from "@tabler/icons-react";
+import { IconChevronDown, IconLogout, IconTrash } from "@tabler/icons-react";
 import cx from "clsx";
 import {
   Avatar,
@@ -20,7 +10,6 @@ import {
   Menu,
   Text,
   UnstyledButton,
-  useMantineTheme,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import classes from "./navbar.module.css";
@@ -28,15 +17,7 @@ import { Link, useNavigate } from "@tanstack/react-router";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../../app/firebase";
 
-const user = {
-  name: "Jane Spoonfighter",
-  email: "janspoon@fighter.dev",
-  image:
-    "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png",
-};
-
 export function Navbar() {
-  const theme = useMantineTheme();
   const [opened, { open, close }] = useDisclosure(false);
   const [userMenuOpened, setUserMenuOpened] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -50,7 +31,12 @@ export function Navbar() {
     return () => unsubscribe();
   }, []);
 
-  console.log(isLoggedIn);
+  const user = {
+    name: "Jane Spoonfighter",
+    email: "janspoon@fighter.dev",
+    image:
+      "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png",
+  };
 
   const handleLogout = (e: React.MouseEvent) => {
     e.preventDefault();
@@ -137,63 +123,12 @@ export function Navbar() {
             </Menu.Target>
             <Menu.Dropdown>
               <Menu.Item
-                leftSection={
-                  <IconHeart
-                    size={16}
-                    color={theme.colors.red[6]}
-                    stroke={1.5}
-                  />
-                }
-              >
-                Liked posts
-              </Menu.Item>
-              <Menu.Item
-                leftSection={
-                  <IconStar
-                    size={16}
-                    color={theme.colors.yellow[6]}
-                    stroke={1.5}
-                  />
-                }
-              >
-                Saved posts
-              </Menu.Item>
-              <Menu.Item
-                leftSection={
-                  <IconMessage
-                    size={16}
-                    color={theme.colors.blue[6]}
-                    stroke={1.5}
-                  />
-                }
-              >
-                Your comments
-              </Menu.Item>
-
-              <Menu.Label>Settings</Menu.Label>
-              <Menu.Item leftSection={<IconSettings size={16} stroke={1.5} />}>
-                Account settings
-              </Menu.Item>
-              <Menu.Item
-                leftSection={<IconSwitchHorizontal size={16} stroke={1.5} />}
-              >
-                Change account
-              </Menu.Item>
-              <Menu.Item
                 leftSection={<IconLogout size={16} stroke={1.5} />}
                 onClick={handleLogout}
               >
                 Logout
               </Menu.Item>
-
               <Menu.Divider />
-
-              <Menu.Label>Danger zone</Menu.Label>
-              <Menu.Item
-                leftSection={<IconPlayerPause size={16} stroke={1.5} />}
-              >
-                Pause subscription
-              </Menu.Item>
               <Menu.Item
                 color="red"
                 leftSection={<IconTrash size={16} stroke={1.5} />}
