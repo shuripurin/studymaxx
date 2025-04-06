16513eef2665dcb59a734973e3b1b854a9e84de4
paul-nguyen96 <paul121596@icloud.com>
2025-04-06 09:17:40 -0500

    add dashboard embededd

---
+++ client/src/components/dashEmbed.tsx
@@ -0,0 +1,11 @@
+function DashEmbed() {
+  return (
+    <iframe
+      src="https://c680-128-193-154-97.ngrok-free.app/dashboard/"
+      style={{ width: "100%", height: "600px", border: "none" }}
+      title="Dash Application"
+    />
+  );
+}
+
+export default DashEmbed;
