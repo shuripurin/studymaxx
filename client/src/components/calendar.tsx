996e5f183eef4743fba245aff416ac4f41e9437a
paul-nguyen96 <paul121596@icloud.com>
2025-04-06 14:14:21 -0500

    update new src

--- client/src/components/calendar.tsx
+++ client/src/components/calendar.tsx
@@ -2,7 +2,7 @@ function Calendar() {
   return (
     <div>
       <iframe
-        src="https://calendar.google.com/calendar/embed?src=paul.nguyen.swe%40gmail.com&ctz=America%2FChicago"
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FChicago&showPrint=0&src=cGF1bC5uZ3V5ZW4uc3dlQGdtYWlsLmNvbQ&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%23039BE5&color=%230B8043"
         width="500"
         height="600"
       ></iframe>
